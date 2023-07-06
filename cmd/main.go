2a542b6e8a0a0e50887ffaeca4f4fa63b30db640
soheil <soheilkhaledabdi@gmail.com>
2023-07-06 11:20:50 -0400

    use pkg logger

--- cmd/main.go
+++ cmd/main.go
@@ -1,19 +1,20 @@
 package main
 
 import (
-	"log"
-
 	"github.com/Asrez/TShirtGoAPI/api"
 	"github.com/Asrez/TShirtGoAPI/data/db"
 	"github.com/Asrez/TShirtGoAPI/config"
+	"github.com/Asrez/TShirtGoAPI/pkg/logging"
 )
 func main(){
 	cfg := config.GetConfig()
+	logger := logging.NewLogger(cfg)
 	err := db.InitDb(cfg)
 	defer db.CloseDb()
 
 	if err != nil {
-		log.Fatal("Connection to database failed error :" , err)
+		// log.Fatal("Connection to database failed error :" , err)
+		logger.Fatal(logging.Postgres , logging.Startup , err.Error() , nil)
 	}
 
 	api.InitServer()
