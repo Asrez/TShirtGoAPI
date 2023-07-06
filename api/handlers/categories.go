fc8c295e20b099629cee2a28343177cccb1b6d09
soheil <soheilkhaledabdi@gmail.com>
2023-07-06 15:21:49 -0400

    edit code

--- api/handlers/categories.go
+++ api/handlers/categories.go
@@ -2,6 +2,9 @@ package handlers
 
 import (
 	"net/http"
+
+	"github.com/Asrez/TShirtGoAPI/data/db"
+	"github.com/Asrez/TShirtGoAPI/data/models"
 	"github.com/gin-gonic/gin"
 )
 
@@ -14,7 +17,22 @@ func NewCategoriesHandler() *Categories{
 	return &Categories{}
 }
 
-func (u *Categories) Categories(c *gin.Context) {
-	c.JSON(http.StatusOK,"Categories ... ")
-	return
+func (c *Categories) Categories(ctx *gin.Context) {
+	db := db.GetDb()
+
+	var brands []models.Brands
+	result := db.Find(&brands)
+	if result.Error != nil {
+		// Handle the error if any
+		ctx.JSON(http.StatusInternalServerError, gin.H{
+			"result": "error",
+			"message": result.Error.Error(),
+		})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, gin.H{
+		"result": "success",
+		"data":   brands,
+	})
 }
