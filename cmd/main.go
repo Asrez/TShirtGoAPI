2f03190f41b4f799a4981f2b22b4d05588dad180
soheil <soheilkhaledabdi@gmail.com>
2023-07-06 11:04:56 -0400

    add code main

--- cmd/main.go
+++ cmd/main.go
@@ -1,3 +1,19 @@
 package main
 
-func main(){}
+import (
+	"log"
+
+	"github.com/Asrez/TShirtGoAPI/api"
+	"github.com/Asrez/TShirtGoAPI/data/db"
+)
+func main(){
+	
+	err := db.InitDb()
+	defer db.CloseDb()
+
+	if err != nil {
+		log.Fatal("Connection to database failed error :" , err)
+	}
+
+	api.InitServer()
+}
