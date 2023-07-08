f42843c7443cb892626154f2b8a2f08eac96104d
soheil <soheilkhaledabdi@gmail.com>
2023-07-08 05:09:27 -0400

    save in redis

--- api/handlers/products.go
+++ api/handlers/products.go
@@ -3,6 +3,7 @@ package handlers
 import (
 	"net/http"
 
+	"github.com/Asrez/TShirtGoAPI/data/cache"
 	"github.com/Asrez/TShirtGoAPI/data/db"
 	"github.com/Asrez/TShirtGoAPI/data/models"
 	"github.com/gin-gonic/gin"
@@ -19,19 +20,25 @@ func NewProductsHandler() *Products{
 
 func (c *Products) Products(ctx *gin.Context) {
 	db := db.GetDb()
+	redis := cache.GetRedis()
+
+	values , err := cache.Get(redis , "products")
+
+	if err != nil {
+		ctx.JSON(http.StatusOK, gin.H{
+			"result": "success",
+			"data":   values,
+		})
+	}
 
 	var Products []models.Products
-	err := db.Preload("Category").Preload("Brand").Preload("Size").Preload("Color").Preload("Material").Find(&Products).Error
+	err = db.Preload("Category").Preload("Brand").Preload("Size").Preload("Color").Preload("Material").Find(&Products).Error
 	if err != nil {
+		cache.Set(redis,"products",err,10000)
 		ctx.JSON(http.StatusInternalServerError, gin.H{
 			"result": "error",
 			"message": err,
 		})
 		return
 	}
-
-	ctx.JSON(http.StatusOK, gin.H{
-		"result": "success",
-		"data":   Products,
-	})
 }
